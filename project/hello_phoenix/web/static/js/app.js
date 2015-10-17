a1a71270b4120325a933ceb23a974a521e440c0a
nyoko708 <ckgxd325@yahoo.co.jp>
2015-10-17 22:21:39 +0900

    edit channel

--- project/hello_phoenix/web/static/js/app.js
+++ project/hello_phoenix/web/static/js/app.js
@@ -26,3 +26,25 @@ let chan = socket.channel("rooms:lobby", {})
   chan.join().receive("ok", chan => {
   console.log("Welcome to Phoenix Chat!")
 })
+
+let chatInput         = $("#chat-input")
+let messagesContainer = $("#messages")
+
+let socket = new Socket("/socket")
+socket.connect()
+let chan = socket.channel("rooms:lobby", {})
+
+chatInput.on("keypress", event => {
+  if(event.keyCode === 13){
+     chan.push("new_msg", {body: chatInput.val()})
+     chatInput.val("")
+  }
+})
+
+chan.on("new_msg", payload => {
+    messagesContainer.append(`<br/>[${Date()}] ${payload.body}`)
+})
+
+chan.join().receive("ok", chan => {
+    console.log("Welcome to Phoenix Chat!")
+})
